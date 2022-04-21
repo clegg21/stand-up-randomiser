beeda701a71793830719539c4638c09ad085f355
@richard.clegg <richard.clegg@oddschecker.com>
2022-04-21 10:10:45 +0100

    Initial commit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [attendees, setAttendees] = useState([
+    "Adam",
+    "Alex",
+    "Cammy",
+    "Chloe",
+    "Chris",
+    "Dima",
+    "Glen",
+    "Harry",
+    "Ioannis",
+    "Nick",
+    "Omar",
+    "Panos",
+    "Pavlos",
+    "Rich",
+    "Roberta",
+  ]);
+
+  const [speaker, setSpeaker] = useState("Who's next?!");
+
+  const getRandomSpeaker = () => {
+    const randomNumber = Math.floor(Math.random() * attendees.length);
+    console.log({ randomNumber });
+    setSpeaker(attendees[randomNumber]);
+    setAttendees(
+      attendees.filter(function (value, index, arr) {
+        return index !== randomNumber;
+      })
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">Stand Up Randomiser</h1>
+      <ol className="questions">
+        <li>What did you achieve yesterday?</li>
+        <li>What do you hope you achieve today?</li>
+        <li>Are there any blockers?</li>
+      </ol>
+      <div className="attendees">
+        {attendees.map((attendee) => {
+          return <p>{attendee}</p>;
+        })}
+      </div>
+      <div className="speaker">
+        <strong>{speaker}</strong>
+        <button onClick={getRandomSpeaker}>Randomise</button>
+      </div>
     </div>
   );
 }
